a310691750e5a1b5c6f1e2498e1a983ea6e18971
Xiaoli Lu <luxiaoli2002@qq.com>
2019-07-20 15:35:10 +0800

    baidu AI

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,12 +5,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MY.Web.Models;
+using Baidu.Aip;
 
 namespace MY.Web.Controllers
 {
     public class HomeController : Controller
     {
+         string APP_ID = "16162924";
+        string API_KEY = "0HGntNQWcLernoX1QA2ILFcA";
+        string SECRET_KEY = "ugNveTHaBPInxv2t0304COOtlIKQ7rIH";
+        Baidu.Aip.Face.Face client;
+
+        public HomeController(){
+            client = new Baidu.Aip.Face.Face(API_KEY,SECRET_KEY);
+        }
         public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Register()
         {
             return View();
         }
